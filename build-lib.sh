517e6257d8d3ef95b6fe65af7cc1b2e463588571
GS <guillaume.solignac@polytechnique.edu>
2020-11-28 19:42:47 +0100

    Fix typo that made us build everything

--- build-lib.sh
+++ build-lib.sh
@@ -1,7 +1,7 @@
 echo "Building lib"
 mkdir build
 cd build
-cmake ../mgba -DLIBMBA_ONLY:bool=TRUE -DBUILD_PYTHON:bool=TRUE
+cmake ../mgba -DLIBMGBA_ONLY:bool=TRUE -DBUILD_PYTHON:bool=TRUE
 make
 cd ..
 cp -r build/python/lib.linux-x86_64-3.8/mgba server/mgba
