659037a9248329492c53e6b4bca589c8b30369e1
GS <guillaume.solignac@polytechnique.edu>
2020-11-29 11:50:19 +0100

    Clean server

--- server/server.py
+++ server/server.py
@@ -4,37 +4,9 @@ import websockets
 import time
 from io import BytesIO
 
-
-print("Loading game")
-c = core.load_path("spm.gba")
-
-dims = c.desired_video_dimensions()
-print("Desired dimensions : ", dims)
-i = image.Image(*dims)
-
-c.set_video_buffer(i)
-c.reset()
-
 screen_pool = set()
 
-commands = [
-    c.KEY_A,
-    c.KEY_B,
-    c.KEY_SELECT,
-    c.KEY_START,
-    c.KEY_RIGHT,
-    c.KEY_LEFT,
-    c.KEY_UP,
-    c.KEY_DOWN,
-    c.KEY_R,
-    c.KEY_L,
-]
-
-
-async def say_after(delay, what):
-    await asyncio.sleep(delay)
-    print(what)
-
+commands = []
 
 async def serve(websocket, path):
     co_type = await websocket.recv()
@@ -60,13 +32,10 @@ async def serve(websocket, path):
             except:
                 again = False
 
-
-print("Starting websocket")
-
-
 async def frame_loop():
     while True:
         compute_time = 0
+        print("Frame")
         if len(screen_pool) > 0:
             t1 = time.clock_gettime_ns(time.CLOCK_REALTIME)
 
@@ -99,28 +68,34 @@ async def frame_loop():
             # print("Time to get frame is ", t2 - t1)
         await asyncio.sleep(max(0, 0.03 - compute_time / 1000000000))
 
-
 async def main():
     print(f"started at {time.strftime('%X')}")
+    print("Starting websocket")
     server = websockets.serve(serve, None, 8100)
     await asyncio.gather(frame_loop(), server)
     print(f"finished at {time.strftime('%X')}")
 
+if __name__ == "__main__":
+    print("Loading game")
+    c = core.load_path("spm.gba")
 
-asyncio.run(main())
+    commands = [
+        c.KEY_A,
+        c.KEY_B,
+        c.KEY_SELECT,
+        c.KEY_START,
+        c.KEY_RIGHT,
+        c.KEY_LEFT,
+        c.KEY_UP,
+        c.KEY_DOWN,
+        c.KEY_R,
+        c.KEY_L,
+    ]
 
-# for _ in range(300):
-#     c.run_frame()
-# c.add_keys(c.KEY_DOWN)
+    dims = c.desired_video_dimensions()
+    print("Desired dimensions : ", dims)
+    i = image.Image(*dims)
 
-# for _ in range(300):
-#     c.add_keys(c.KEY_DOWN)
-#     c.run_frame()
-
-# c.clear_keys(c.KEY_DOWN)
-# f = open('out.png', 'wb')
-# i.save_png(f)
-# f.close()
-
-
-# for x in range(800): c.run_frame()
+    c.set_video_buffer(i)
+    c.reset()
+    asyncio.run(main())
